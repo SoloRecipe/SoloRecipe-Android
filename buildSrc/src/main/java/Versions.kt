d198f7740de243f0ee89ba8f637814cc72a054d3
hyeonseo <s21035@gsm.hs.kr>
2023-05-17 09:49:26 +0900

    :wrench: :: Versions

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,28 @@
+object Versions {
+    const val GRADLE_ANDROID = "8.0.0"
+    const val GRADLE_KOTLIN = "1.8.0"
+    const val GRADLE_KTLINT = "11.3.2"
+
+    const val CORE_KTX = "1.10.0"
+    const val APPCOMPAT = "1.6.1"
+    const val COMPOSE_RUNTIME = "1.4.3"
+    const val COMPOSE_MATERIAL3 = "1.1.0"
+    const val ACTIVITY_COMPOSE = "1.7.1"
+    const val COMPOSE_TOOLING = "1.4.2"
+    const val COMPOSE_UI = "1.4.3"
+    const val NAVIGATION_COMPOSE = "2.5.3"
+    const val COMPOSE_UI_UTIL = "1.4.3"
+    const val LIFECYCLE = "2.6.1"
+    const val HILT_NAVIGATION_COMPOSE = "1.0.0"
+    const val DATASTORE = "1.0.0"
+
+    const val MATERIAL = "1.9.0"
+    const val HILT = "2.44"
+    const val OKHTTP = "4.11.0"
+
+    const val RETROFIT = "2.9.0"
+
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.5"
+    const val ESPRESSO_CORE = "3.5.1"
+}
