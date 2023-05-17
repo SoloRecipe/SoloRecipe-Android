a809d3df2b03eb495ee143138e504cffe691dc1f
hyeonseo <s21035@gsm.hs.kr>
2023-05-17 10:17:00 +0900

    :sparkles: :: domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id(ProjectProperties.Gradle.LIBRARY)
+    id(ProjectProperties.Gradle.KOTLIN)
+}
+
+android {
+    namespace = ProjectProperties.NameSpace.DOMAIN
+    compileSdk = ProjectProperties.Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = ProjectProperties.Versions.MIN_SDK
+
+        testInstrumentationRunner = ProjectProperties.Test.TEST_RUNNER
+        consumerProguardFiles(ProjectProperties.Files.CONSUMER_PROGUARD)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(ProjectProperties.Files.DEFAULT_PROGUARD), ProjectProperties.Files.PROGUARD
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = ProjectProperties.Versions.JAVA_VERSION
+        targetCompatibility = ProjectProperties.Versions.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = ProjectProperties.Versions.JVM_TARGET
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.UnitTest.JUNIT)
+    implementation(Dependencies.AndroidTest.ANDROID_JUNIT)
+    implementation(Dependencies.AndroidTest.ESPRESSO_CORE)
+}
