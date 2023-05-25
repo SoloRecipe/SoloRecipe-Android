0c76d307c1489a9c01458b6227f6a3a240924055
hyeonseo <s21035@gsm.hs.kr>
2023-05-25 13:52:36 +0900

    :sparkles: :: SoloRecipeCommentItem

---
+++ design_system/src/main/java/com/project/design_system/component/SoloRecipeCommentItem.kt
@@ -0,0 +1,44 @@
+package com.project.design_system.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.project.design_system.theme.Body4
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
+
+@Composable
+fun SoloRecipeCommentItem(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    title: String,
+    content: String
+) {
+    Row(modifier = modifier) {
+        GlideImage(
+            modifier = Modifier
+                .size(
+                    width = 30.dp,
+                    height = 30.dp
+                )
+                .clip(CircleShape),
+            imageModel = { imageUrl },
+            imageOptions = ImageOptions(contentScale = ContentScale.Crop)
+        )
+        Spacer(modifier = Modifier.width(18.dp))
+        Column(modifier = modifier) {
+            Body4(text = title)
+            Spacer(modifier = Modifier.height(4.dp))
+            Body4(text = content)
+        }
+    }
+}
