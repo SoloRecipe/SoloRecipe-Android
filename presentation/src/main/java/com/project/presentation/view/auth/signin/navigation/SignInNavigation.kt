0a829f45fb68391c849eb76322d8846542d1f9af
hyeonseo <s21035@gsm.hs.kr>
2023-05-25 11:07:38 +0900

    :fire: :: 사용되지 않는 navigation 삭제

--- presentation/src/main/java/com/project/presentation/view/auth/signin/navigation/SignInNavigation.kt
+++ presentation/src/main/java/com/project/presentation/view/auth/signin/navigation/SignInNavigation.kt
@@ -1,14 +1,9 @@
 package com.project.presentation.view.auth.signin.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
 const val signInRoute = "sign_in_route"
-
-fun NavController.navigateToSignIn() {
-    this.navigate(signInRoute)
-}
 
 fun NavGraphBuilder.signInScreen() {
     composable(signInRoute) {
