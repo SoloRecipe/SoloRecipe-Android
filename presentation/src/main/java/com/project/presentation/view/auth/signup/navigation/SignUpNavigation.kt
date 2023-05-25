f38d8ce0373c6f1a1205c1ad235e79f86dddf762
hyeonseo <s21035@gsm.hs.kr>
2023-05-25 15:28:10 +0900

    :sparkles: :: SignUpNavigation

---
+++ presentation/src/main/java/com/project/presentation/view/auth/signup/navigation/SignUpNavigation.kt
@@ -0,0 +1,18 @@
+package com.project.presentation.view.auth.signup.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.project.presentation.view.auth.signup.SignUpScreen
+
+const val signUpRoute = "sign_up_route"
+
+fun NavController.navigateToSignUp() {
+    this.navigate(signUpRoute)
+}
+
+fun NavGraphBuilder.signUpScreen() {
+    composable(signUpRoute) {
+        SignUpScreen()
+    }
+}
