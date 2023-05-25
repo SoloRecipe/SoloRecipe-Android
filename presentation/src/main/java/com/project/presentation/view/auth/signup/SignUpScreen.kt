3816a65a90faf1c391d71bf3ca3344635864c5de
hyeonseo <s21035@gsm.hs.kr>
2023-05-25 16:18:50 +0900

    :fire: :: Preview 삭제

--- presentation/src/main/java/com/project/presentation/view/auth/signup/SignUpScreen.kt
+++ presentation/src/main/java/com/project/presentation/view/auth/signup/SignUpScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.design_system.component.SoloRecipeButton
 import com.project.design_system.component.SoloRecipeTextField
@@ -172,10 +171,4 @@ fun SignUpHeader(
             text = "회원가입"
         )
     }
-}
-
-@Preview
-@Composable
-fun PreviewSignUpScreen() {
-    SignUpScreen()
 }
