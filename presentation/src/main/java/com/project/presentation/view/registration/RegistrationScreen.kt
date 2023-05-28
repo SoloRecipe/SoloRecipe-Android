ddbd8422cafe1a269227b5e74b7e609e034177c7
박성현 <pjhomg5530@naver.com>
2023-05-28 18:35:13 +0900

    :truck: 함수명 변경

--- presentation/src/main/java/com/project/presentation/view/registration/RegistrationScreen.kt
+++ presentation/src/main/java/com/project/presentation/view/registration/RegistrationScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
