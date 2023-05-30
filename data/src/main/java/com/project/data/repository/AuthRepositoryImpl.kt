5d85e154e2bb7d674d84fc6e6d0b695eddd8f1e4
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 15:42:51 +0900

    :sparkles: :: Repository 및 Module 로직

---
+++ data/src/main/java/com/project/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.project.data.repository
+
+import com.project.data.remote.datasource.auth.AuthDataSource
+import com.project.data.remote.model.request.asSignInRequest
+import com.project.data.remote.model.response.asSignInResponseModel
+import com.project.domain.model.auth.request.SignInRequestModel
+import com.project.domain.model.auth.response.SignInResponseModel
+import com.project.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val authDataSource: AuthDataSource
+) : AuthRepository {
+    override suspend fun signIn(signInRequestModel: SignInRequestModel): SignInResponseModel =
+        authDataSource.signIn(signInRequestModel.asSignInRequest()).asSignInResponseModel()
+}
---
+++ domain/src/main/java/com/project/domain/repository/AuthRepository.kt
@@ -0,0 +1,8 @@
+package com.project.domain.repository
+
+import com.project.domain.model.auth.request.SignInRequestModel
+import com.project.domain.model.auth.response.SignInResponseModel
+
+interface AuthRepository {
+    suspend fun signIn(signInRequestModel: SignInRequestModel): SignInResponseModel
+}
