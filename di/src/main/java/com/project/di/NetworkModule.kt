13b35ed96bdb611899fe9fdcf2c55ed12e23ade6
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 14:21:20 +0900

    :sparkles: :: Hilt annotation 추가

--- di/src/main/java/com/project/di/NetworkModule.kt
+++ di/src/main/java/com/project/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package com.project.di
 
 import com.project.data.remote.network.AuthApi
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     @Singleton
