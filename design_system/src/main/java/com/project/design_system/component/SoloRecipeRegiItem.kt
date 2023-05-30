5aca6dd0439f70568c3af413a2795b14ffefce7b
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 09:58:02 +0900

    :sparkles: :: SoloRecipeRegiItem

---
+++ design_system/src/main/java/com/project/design_system/component/SoloRecipeRegiItem.kt
@@ -0,0 +1,79 @@
+package com.project.design_system.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.project.design_system.R
+import com.project.design_system.theme.Body4
+import com.project.design_system.theme.SoloRecipeColor
+import com.project.design_system.theme.SoloRecipeTypography.body4
+
+@Composable
+fun SoloRecipeRegiItem(
+    modifier: Modifier = Modifier,
+    value: String,
+    hint: String,
+    image: Painter? = null,
+    onValueChanged: (String) -> Unit
+) {
+    Row(modifier = Modifier.height(70.dp)) {
+        Box(
+            modifier = Modifier
+                .width(80.dp)
+                .fillMaxHeight()
+                .background(
+                    color = SoloRecipeColor.Secondary10,
+                    shape = RoundedCornerShape(8.dp)
+                )
+        ) {
+            Image(
+                painter = image ?: painterResource(id = R.drawable.ic_camera),
+                contentDescription = "image",
+                modifier = modifier.align(Alignment.Center)
+            )
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        BasicTextField(
+            modifier = modifier
+                .fillMaxHeight()
+                .border(
+                    width = 1.dp,
+                    color = SoloRecipeColor.Secondary10,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(10.dp),
+            cursorBrush = SolidColor(SoloRecipeColor.Primary10),
+            value = value,
+            textStyle = body4.merge(TextStyle(color = SoloRecipeColor.Black)),
+            onValueChange = onValueChanged,
+            decorationBox = { innerTextField ->
+                Box(modifier = modifier) {
+                    if (value.isEmpty()) {
+                        Body4(
+                            text = hint,
+                            textColor = SoloRecipeColor.Secondary10,
+                        )
+                    }
+                    innerTextField()
+                }
+            }
+        )
+    }
+}
