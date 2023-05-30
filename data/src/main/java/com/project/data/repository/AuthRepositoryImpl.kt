136e1ba2d4f7945abaec1f77db1aacbbe24264ab
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 20:02:49 +0900

    :sparkles: :: Repository 로직

--- data/src/main/java/com/project/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/com/project/data/repository/AuthRepositoryImpl.kt
@@ -2,8 +2,10 @@ package com.project.data.repository
 
 import com.project.data.remote.datasource.auth.AuthDataSource
 import com.project.data.remote.model.request.asSignInRequest
+import com.project.data.remote.model.request.asSignUpRequest
 import com.project.data.remote.model.response.asSignInResponseModel
 import com.project.domain.model.auth.request.SignInRequestModel
+import com.project.domain.model.auth.request.SignUpRequestModel
 import com.project.domain.model.auth.response.SignInResponseModel
 import com.project.domain.repository.AuthRepository
 import javax.inject.Inject
@@ -13,4 +15,8 @@ class AuthRepositoryImpl @Inject constructor(
 ) : AuthRepository {
     override suspend fun signIn(signInRequestModel: SignInRequestModel): SignInResponseModel =
         authDataSource.signIn(signInRequestModel.asSignInRequest()).asSignInResponseModel()
+
+    override suspend fun signUp(signUpRequestModel: SignUpRequestModel) {
+        authDataSource.signUp(signUpRequestModel.asSignUpRequest())
+    }
 }
--- domain/src/main/java/com/project/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/project/domain/repository/AuthRepository.kt
@@ -1,8 +1,11 @@
 package com.project.domain.repository
 
 import com.project.domain.model.auth.request.SignInRequestModel
+import com.project.domain.model.auth.request.SignUpRequestModel
 import com.project.domain.model.auth.response.SignInResponseModel
 
 interface AuthRepository {
     suspend fun signIn(signInRequestModel: SignInRequestModel): SignInResponseModel
+
+    suspend fun signUp(signUpRequestModel: SignUpRequestModel)
 }
