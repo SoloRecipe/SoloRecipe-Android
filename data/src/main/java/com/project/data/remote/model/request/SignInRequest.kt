d2da5b54294794dc3801108a356e52eab261e76c
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 15:41:14 +0900

    :sparkles: :: SignInModel 및 extension 추가

--- data/src/main/java/com/project/data/remote/model/request/SignInRequest.kt
+++ data/src/main/java/com/project/data/remote/model/request/SignInRequest.kt
@@ -1,7 +1,15 @@
 package com.project.data.remote.model.request
 
+import com.project.domain.model.auth.request.SignInRequestModel
+
 data class SignInRequest(
     val email: String,
     val password: String,
     val nickname: String
+)
+
+fun SignInRequestModel.asSignInRequest() = SignInRequest(
+    email = email,
+    password = password,
+    nickname = nickname
 )
