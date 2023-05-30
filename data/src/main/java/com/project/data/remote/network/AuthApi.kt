3122ad91562b66fafe67bd22786690b666088cea
hyeonseo <s21035@gsm.hs.kr>
2023-05-30 14:09:13 +0900

    :sparkles: :: SignInResponse 추가

--- data/src/main/java/com/project/data/remote/network/AuthApi.kt
+++ data/src/main/java/com/project/data/remote/network/AuthApi.kt
@@ -1,6 +1,7 @@
 package com.project.data.remote.network
 
 import com.project.data.remote.model.request.SignInRequest
+import com.project.data.remote.model.response.SignInResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -8,5 +9,5 @@ interface AuthApi {
     @POST("login")
     suspend fun signIn(
         @Body body: SignInRequest
-    )
+    ): SignInResponse
 }
