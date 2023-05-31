d2522c757daae888081aa0516d56885b323219b7
hyeonseo <s21035@gsm.hs.kr>
2023-05-31 11:17:10 +0900

    :sparkles: :: PagingSource 로직

---
+++ data/src/main/java/com/project/data/remote/datasource/recipe/RecipePagingSource.kt
@@ -0,0 +1,42 @@
+package com.project.data.remote.datasource.recipe
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.project.data.remote.model.response.RecipeResponse
+import com.project.data.remote.network.RecipeApi
+import com.project.data.remote.util.safeApiCall
+
+class RecipePagingSource(
+    private val recipeApi: RecipeApi,
+    private val filter: String? = null
+) : PagingSource<Int, RecipeResponse>() {
+    override fun getRefreshKey(state: PagingState<Int, RecipeResponse>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RecipeResponse> {
+        val page = params.key ?: 1
+        val response = safeApiCall {
+            if (filter == "recommend") {
+                recipeApi.getAllRecipes(
+                    page = page,
+                    size = params.loadSize
+                )
+            } else {
+                recipeApi.getAllRecipes(
+                    page = page,
+                    size = params.loadSize
+                )
+            }
+        }
+
+        return LoadResult.Page(
+            data = response.recipeList,
+            prevKey = if (page == 1) null else page - 1,
+            nextKey = if (response.recipeList.isEmpty()) null else page + 1
+        )
+    }
+}
