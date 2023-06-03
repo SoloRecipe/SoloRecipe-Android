7e5d1779fb96a6bec5b0af7905b88a940517ff8e
박성현 <pjhomg5530@naver.com>
2023-06-03 15:56:34 +0900

    :sparkles: ProfileApi 생성

---
+++ data/src/main/java/com/project/data/remote/network/ProfileApi.kt
@@ -0,0 +1,21 @@
+package com.project.data.remote.network
+
+import com.project.data.remote.model.request.ProfileRequest
+import com.project.data.remote.model.response.ProfilesResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+
+interface ProfileApi {
+    @GET("user")
+    suspend fun getUserInfo(): ProfilesResponse
+
+    @PATCH("user")
+    suspend fun renameUserName(
+        @Body body: ProfileRequest
+    )
+
+    @DELETE("user")
+    suspend fun deleteUserInfo()
+}
