cfb5cbf5d11f08d576325dfdf8ecf1da7f6914b1
박성현 <pjhomg5530@naver.com>
2023-06-03 17:50:13 +0900

    :sparkles: ProfileDataSource 생성

---
+++ data/src/main/java/com/project/data/remote/datasource/profile/ProfileDataSource.kt
@@ -0,0 +1,12 @@
+package com.project.data.remote.datasource.profile
+
+import com.project.data.remote.model.request.ProfileRequest
+import com.project.data.remote.model.response.ProfilesResponse
+
+interface ProfileDataSource {
+    suspend fun getUserInfo(): ProfilesResponse
+
+    suspend fun renameUserName(body: ProfileRequest)
+
+    suspend fun deleteUserInfo()
+}
