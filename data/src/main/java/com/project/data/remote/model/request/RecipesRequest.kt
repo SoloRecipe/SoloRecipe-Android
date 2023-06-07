0b0fc395027a931a11f9b2622b4832b09841dccc
박성현 <pjhomg5530@naver.com>
2023-06-07 16:06:37 +0900

    :sparkles: mapper 구현

--- data/src/main/java/com/project/data/remote/model/request/RecipesRequest.kt
+++ data/src/main/java/com/project/data/remote/model/request/RecipesRequest.kt
@@ -1,7 +1,15 @@
 package com.project.data.remote.model.request
 
+import com.project.domain.model.recipe.RecipesRequestModel
+
 data class RecipesRequest(
     val name: String,
     val thumbnail: String,
     val recipeProcess: List<RecipeRequest>
+)
+
+fun RecipesRequestModel.asRecipesRequest() = RecipesRequest(
+    name = name,
+    thumbnail = thumbnail,
+    recipeProcess = recipeProcess.map { it.asRecipeRequest() }
 )
