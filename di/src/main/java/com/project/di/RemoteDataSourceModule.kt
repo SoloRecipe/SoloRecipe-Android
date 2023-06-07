c5adf0492842226cd2587098a4b135ffc2f2629d
hyeonseo <s21035@gsm.hs.kr>
2023-06-08 08:41:04 +0900

    :sparkles: :: DataSource DI

--- di/src/main/java/com/project/di/RemoteDataSourceModule.kt
+++ di/src/main/java/com/project/di/RemoteDataSourceModule.kt
@@ -2,10 +2,14 @@ package com.project.di
 
 import com.project.data.remote.datasource.auth.AuthDataSource
 import com.project.data.remote.datasource.auth.AuthDataSourceImpl
+import com.project.data.remote.datasource.like.LikeDataSource
+import com.project.data.remote.datasource.like.LikeDataSourceImpl
 import com.project.data.remote.datasource.profile.ProfileDataSource
 import com.project.data.remote.datasource.profile.ProfileDataSourceImpl
 import com.project.data.remote.datasource.recipe.RecipeDataSource
 import com.project.data.remote.datasource.recipe.RecipeDataSourceImpl
+import com.project.data.remote.datasource.review.ReviewDataSource
+import com.project.data.remote.datasource.review.ReviewDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -28,4 +32,14 @@ interface RemoteDataSourceModule {
     fun bindsProfileDataSource(
         profileDataSourceImpl: ProfileDataSourceImpl
     ): ProfileDataSource
+
+    @Binds
+    fun bindsReviewDataSource(
+        reviewDataSourceImpl: ReviewDataSourceImpl
+    ): ReviewDataSource
+
+    @Binds
+    fun bindsLikeDataSource(
+        likeDataSourceImpl: LikeDataSourceImpl
+    ): LikeDataSource
 }
