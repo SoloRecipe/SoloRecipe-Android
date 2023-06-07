be89214ac5fbcc2a8b29e7638194ed27545c773b
hyeonseo <s21035@gsm.hs.kr>
2023-06-07 15:42:20 +0900

    :sparkles: :: ViewModel 로직

---
+++ presentation/src/main/java/com/project/presentation/viewmodel/recipe/RecipeViewModel.kt
@@ -0,0 +1,42 @@
+package com.project.presentation.viewmodel.recipe
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.project.domain.model.auth.response.RecipeResponseModel
+import com.project.domain.usecase.GetAllRecipesUseCase
+import com.project.domain.usecase.GetRecommendRecipesUseCase
+import com.project.domain.usecase.SearchRecipeUseCase
+import com.project.presentation.view.main.RECOMMEND
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class RecipeViewModel @Inject constructor(
+    private val getAllRecipesUseCase: GetAllRecipesUseCase,
+    private val getRecommendRecipesUseCase: GetRecommendRecipesUseCase,
+    private val searchRecipeUseCase: SearchRecipeUseCase
+) : ViewModel() {
+    fun getRecipes(type: Int): Flow<PagingData<RecipeResponseModel>> {
+        val recipes = if (type == RECOMMEND) {
+            getRecommendRecipesUseCase().cachedIn(viewModelScope)
+        } else {
+            getAllRecipesUseCase().cachedIn(viewModelScope)
+        }
+        Log.d("recipes", recipes.toString())
+        return recipes
+    }
+
+    fun searchRecipe(name: String) {
+        viewModelScope.launch {
+            searchRecipeUseCase(name)
+                .onSuccess {
+                    Log.d("searchRecipe", it.toString())
+                }.onFailure {
+                    Log.d("searchRecipe", it.message.toString())
+                }
+        }
+    }
+}
