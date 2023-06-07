f2433d5afe575167bcd8548a35b6d26b7112d534
박성현 <pjhomg5530@naver.com>
2023-06-07 15:39:43 +0900

    :sparkles: DataSource 구현

--- data/src/main/java/com/project/data/remote/datasource/recipe/RecipeDataSource.kt
+++ data/src/main/java/com/project/data/remote/datasource/recipe/RecipeDataSource.kt
@@ -1,7 +1,10 @@
 package com.project.data.remote.datasource.recipe
 
+import com.project.data.remote.model.request.RecipesRequest
 import com.project.data.remote.model.response.RecipeResponse
 
 interface RecipeDataSource {
     suspend fun searchRecipe(name: String): RecipeResponse
+
+    suspend fun createRecipe(body: RecipesRequest)
 }
