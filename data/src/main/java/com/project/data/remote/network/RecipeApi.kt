dd5344cf01be1f1d4a8974001a0c7473723c3010
hyeonseo <s21035@gsm.hs.kr>
2023-06-07 18:26:10 +0900

    :sparkles: :: 디테일 페이지 remote 로직 작성

--- data/src/main/java/com/project/data/remote/network/RecipeApi.kt
+++ data/src/main/java/com/project/data/remote/network/RecipeApi.kt
@@ -6,6 +6,7 @@ import com.project.data.remote.model.response.RecipesResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RecipeApi {
@@ -29,5 +30,10 @@ interface RecipeApi {
     @POST("recipe")
     suspend fun createRecipe(
         @Body body: RecipesRequest
+    )
+
+    @GET("recipe/detail")
+    suspend fun getRecipeDetail(
+        @Path("idx") index: Long
     )
 }
---
+++ data/src/main/java/com/project/data/remote/network/ReviewApi.kt
@@ -0,0 +1,26 @@
+package com.project.data.remote.network
+
+import com.project.data.remote.model.request.ReviewRequest
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ReviewApi {
+    @POST("review")
+    suspend fun writeReview(
+        @Path("recipe-idx") recipeIndex: Long
+    )
+
+    @PATCH("review")
+    suspend fun modifyReview(
+        @Path("recipe-idx") recipeIndex: Long,
+        @Body body: ReviewRequest
+    )
+
+    @DELETE("review")
+    suspend fun deleteReview(
+        @Path("recipe-idx") recipeIndex: Long
+    )
+}
