1d41c2c8e58a6881f8750dce3f1d63304b6dcdd9
hyeonseo <s21035@gsm.hs.kr>
2023-06-08 10:41:00 +0900

    :sparkles: :: Body 추가

--- domain/src/main/java/com/project/domain/repository/ReviewRepository.kt
+++ domain/src/main/java/com/project/domain/repository/ReviewRepository.kt
@@ -3,7 +3,10 @@ package com.project.domain.repository
 import com.project.domain.model.review.request.ReviewRequestModel
 
 interface ReviewRepository {
-    suspend fun writeReview(recipeIndex: Long)
+    suspend fun writeReview(
+        recipeIndex: Long,
+        body: ReviewRequestModel
+    )
 
     suspend fun modifyReview(
         recipeIndex: Long,
