95ea00e6be2021c0e6a0534c64733d60895eef7c
박성현 <pjhomg5530@naver.com>
2023-06-08 18:04:24 +0900

    :sparkles: DataSource에 modifyProfileImage 생성

--- data/src/main/java/com/project/data/remote/datasource/profile/ProfileDataSource.kt
+++ data/src/main/java/com/project/data/remote/datasource/profile/ProfileDataSource.kt
@@ -1,5 +1,6 @@
 package com.project.data.remote.datasource.profile
 
+import com.project.data.remote.model.request.ProfileImageRequest
 import com.project.data.remote.model.request.ProfileRequest
 import com.project.data.remote.model.response.ProfilesResponse
 
@@ -9,4 +10,6 @@ interface ProfileDataSource {
     suspend fun renameUserName(body: ProfileRequest)
 
     suspend fun deleteUserInfo()
+
+    suspend fun modifyProfileImage(body: ProfileImageRequest)
 }
