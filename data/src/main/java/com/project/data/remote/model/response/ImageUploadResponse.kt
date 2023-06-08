82fbcef8effd1da1b2e171bf3762a99e2061c6b1
박성현 <pjhomg5530@naver.com>
2023-06-08 20:59:02 +0900

    :sparkles: ImageUploadResponse에 mapper 추가

--- data/src/main/java/com/project/data/remote/model/response/ImageUploadResponse.kt
+++ data/src/main/java/com/project/data/remote/model/response/ImageUploadResponse.kt
@@ -1,5 +1,11 @@
 package com.project.data.remote.model.response
 
+import com.project.domain.model.profile.response.ImageUploadResponseModel
+
 data class ImageUploadResponse(
     val url: List<String>
+)
+
+fun ImageUploadResponse.asImageUploadResponseModel() = ImageUploadResponseModel(
+    url = url
 )
