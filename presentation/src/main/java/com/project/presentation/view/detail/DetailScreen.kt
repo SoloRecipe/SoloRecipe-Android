63227e4e07b3a182de2bbbffb7785b1d6253f5fb
hyeonseo <s21035@gsm.hs.kr>
2023-06-09 11:16:41 +0900

    :sparkles: :: UiState로 상태관리

--- presentation/src/main/java/com/project/presentation/view/detail/DetailScreen.kt
+++ presentation/src/main/java/com/project/presentation/view/detail/DetailScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.project.design_system.component.SoloRecipeAppBar
 import com.project.design_system.component.SoloRecipeCommentItem
 import com.project.design_system.component.SoloRecipeStepItem
@@ -41,9 +42,14 @@ import com.project.design_system.theme.Body3
 import com.project.design_system.theme.IcEmptyHeart
 import com.project.design_system.theme.IcFullHeart
 import com.project.design_system.theme.SoloRecipeTheme
+import com.project.domain.model.recipe.response.RecipeDetailResponseModel
+import com.project.domain.model.recipe.response.ReviewModel
 import com.project.domain.model.review.request.ReviewRequestModel
 import com.project.presentation.R
 import com.project.presentation.viewmodel.detail.DetailViewModel
+import com.project.presentation.viewmodel.util.UiState
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun DetailScreen(
@@ -53,71 +59,82 @@ fun DetailScreen(
 ) {
     val recipeIndex = checkNotNull(index)
 
+    val recipeUiState by detailViewModel.recipeUiState.collectAsStateWithLifecycle()
+
     LaunchedEffect(Unit) {
         detailViewModel.getRecipeDetail(recipeIndex)
     }
 
-    Column(modifier = modifier.fillMaxSize()) {
-        SoloRecipeAppBar { }
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .background(SoloRecipeTheme.color.Background)
-                .padding(horizontal = 26.dp)
-                .verticalScroll(rememberScrollState()),
-        ) {
-            Spacer(modifier = modifier.height(16.dp))
-            DetailTitle { detailViewModel.likeRecipe() }
-            Spacer(modifier = modifier.height(26.dp))
-            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-                repeat(5) {
-                    SoloRecipeStepItem(
+    when (val state = recipeUiState) {
+        UiState.Loading -> {}
+        is UiState.Success -> {
+            Column(modifier = modifier.fillMaxSize()) {
+                SoloRecipeAppBar { }
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(SoloRecipeTheme.color.Background)
+                        .padding(horizontal = 26.dp)
+                        .verticalScroll(rememberScrollState()),
+                ) {
+                    Spacer(modifier = modifier.height(16.dp))
+                    DetailTitle(recipeDetail = state.data) { detailViewModel.likeRecipe() }
+                    Spacer(modifier = modifier.height(26.dp))
+                    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                        val recipeProcess = state.data?.recipeProcess
+
+                        repeat(recipeProcess?.size ?: 0) {
+                            SoloRecipeStepItem(
+                                modifier = modifier.fillMaxWidth(),
+                                imageUrl = recipeProcess?.get(it)?.image ?: "",
+                                content = recipeProcess?.get(it)?.description ?: ""
+                            )
+                        }
+                    }
+                    Spacer(modifier = modifier.height(40.dp))
+                    Body3(text = "댓글")
+                    Spacer(modifier = modifier.height(20.dp))
+                    MyComment(index = recipeIndex) { index, content ->
+                        detailViewModel.writeReview(
+                            recipeIndex = recipeIndex,
+                            body = ReviewRequestModel(content = content)
+                        )
+                        detailViewModel.getRecipeDetail(index)
+                    }
+                    Spacer(modifier = modifier.height(30.dp))
+                    Column(
                         modifier = modifier.fillMaxWidth(),
-                        imageUrl = "https://example.com",
-                        content = "돈까스용 돼지고기(등심),밀가루,\n" +
-                                "빵가루,계란,식용유,허브솔트를 \n" +
-                                "준비해주세요"
-                    )
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        repeat(state.data?.reviews?.size ?: 0) {
+                            CommentList(review = state.data?.reviews?.get(it))
+                        }
+                    }
+                    Spacer(modifier = modifier.height(21.dp))
                 }
             }
-            Spacer(modifier = modifier.height(40.dp))
-            Body3(text = "댓글")
-            Spacer(modifier = modifier.height(20.dp))
-            MyComment(index = recipeIndex) { index, content ->
-                detailViewModel.writeReview(
-                    recipeIndex = index,
-                    body = ReviewRequestModel(content = content)
-                )
-            }
-            Spacer(modifier = modifier.height(30.dp))
-            Column(
-                modifier = modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                repeat(5) {
-                    CommentList()
-                }
-            }
-            Spacer(modifier = modifier.height(21.dp))
         }
+        UiState.Unauthorized -> {}
+        UiState.NotFound -> {}
+        else -> {}
     }
 }
 
 @Composable
 fun DetailTitle(
     modifier: Modifier = Modifier,
+    recipeDetail: RecipeDetailResponseModel?,
     onLike: (Boolean) -> Unit
 ) {
     var liked by remember { mutableStateOf(false) }
 
-    Image(
+    GlideImage(
         modifier = modifier
             .fillMaxWidth()
             .height(250.dp)
             .clip(RoundedCornerShape(8.dp)),
-        painter = painterResource(id = R.drawable.ic_logo),
-        contentDescription = "title_image",
-        contentScale = ContentScale.Crop
+        imageModel = { recipeDetail?.thumbnail },
+        imageOptions = ImageOptions(contentScale = ContentScale.Crop)
     )
     Spacer(modifier = modifier.height(9.dp))
     Row(
@@ -126,7 +143,7 @@ fun DetailTitle(
     ) {
         Body0(
             modifier = modifier.weight(1f),
-            text = "10분만에 쉽게 만드는 돈가스돈가스돈가스돈가스돈가스돈가스돈가스돈가스돈가스",
+            text = recipeDetail?.name ?: "",
             maxLines = 2
         )
         Spacer(modifier = modifier.width(30.dp))
@@ -182,13 +199,15 @@ fun MyComment(
 }
 
 @Composable
-fun CommentList(modifier: Modifier = Modifier) {
+fun CommentList(
+    modifier: Modifier = Modifier,
+    review: ReviewModel?
+) {
     Column(modifier = modifier.fillMaxWidth()) {
         SoloRecipeCommentItem(
-            imageUrl = "https://example.com",
-            title = "테스트",
-            content = "감사합니다 레시피 덕분에 돈까스를 성공적으로\n" +
-                    "만들 수 있었습니다!!"
+            imageUrl = "",
+            title = review?.userName ?: "",
+            content = review?.content ?: ""
         )
         Spacer(modifier = modifier.height(5.dp))
         Divider(
