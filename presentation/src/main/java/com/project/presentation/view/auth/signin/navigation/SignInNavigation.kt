19e13989017bdde6274e9a227b6853ff148195eb
hyeonseo <s21035@gsm.hs.kr>
2023-06-09 09:14:17 +0900

    :sparkles: :: SignInNavigation

--- presentation/src/main/java/com/project/presentation/view/auth/signin/navigation/SignInNavigation.kt
+++ presentation/src/main/java/com/project/presentation/view/auth/signin/navigation/SignInNavigation.kt
@@ -1,11 +1,15 @@
 package com.project.presentation.view.auth.signin.navigation
 
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.project.presentation.view.auth.signin.SignInScreen
 
 const val signInRoute = "sign_in_route"
 
+fun NavController.navigateToSignIn() {
+    this.navigate(signInRoute)
+}
 fun NavGraphBuilder.signInScreen(
     navigateToSignUp: () -> Unit,
     navigateToMain: () -> Unit
