18614335214cc7e7533061990213f4d5580c8dc2
hyeonseo <s21035@gsm.hs.kr>
2023-06-09 22:25:57 +0900

    :art: :: PagingSource 기능별 분리

---
+++ data/src/main/java/com/project/data/remote/datasource/recipe/RecommendRecipePagingSource.kt
@@ -0,0 +1,34 @@
+package com.project.data.remote.datasource.recipe
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.project.data.remote.model.response.RecipeResponse
+import com.project.data.remote.network.api.RecipeApi
+import com.project.data.remote.util.safeApiCall
+
+class RecommendRecipePagingSource(
+    private val recipeApi: RecipeApi,
+) : PagingSource<Int, RecipeResponse>() {
+    override fun getRefreshKey(state: PagingState<Int, RecipeResponse>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RecipeResponse> {
+        val page = params.key ?: 0
+        val response = safeApiCall {
+            recipeApi.getRecommendRecipes(
+                page = page,
+                size = params.loadSize
+            )
+        }
+
+        return LoadResult.Page(
+            data = response.recipeList,
+            prevKey = if (page == 0) null else page - 1,
+            nextKey = if (response.recipeList.isEmpty()) null else page + 1
+        )
+    }
+}
