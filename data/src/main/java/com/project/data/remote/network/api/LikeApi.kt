2b58fb0e4e45a2ea18e652206cbff4c398319e59
hyeonseo <s21035@gsm.hs.kr>
2023-06-12 07:59:51 +0900

    :sparkles: :: endpoint에 path 추가

--- data/src/main/java/com/project/data/remote/network/api/LikeApi.kt
+++ data/src/main/java/com/project/data/remote/network/api/LikeApi.kt
@@ -1,8 +1,11 @@
 package com.project.data.remote.network.api
 
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface LikeApi {
-    @POST("like")
-    suspend fun likeRecipe()
+    @POST("like/{recipe-idx}")
+    suspend fun likeRecipe(
+        @Path("recipe-idx") index: Long
+    )
 }
