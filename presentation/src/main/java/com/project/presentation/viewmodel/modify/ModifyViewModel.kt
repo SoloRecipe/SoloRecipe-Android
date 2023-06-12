4d128e11f6d432f16035d6ca2f1794ebc4f6a5a2
hyeonseo <s21035@gsm.hs.kr>
2023-06-13 00:26:52 +0900

    :sparkles: :: 레시피 상세 조회 추가

--- presentation/src/main/java/com/project/presentation/viewmodel/modify/ModifyViewModel.kt
+++ presentation/src/main/java/com/project/presentation/viewmodel/modify/ModifyViewModel.kt
@@ -3,17 +3,23 @@ package com.project.presentation.viewmodel.modify
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.project.domain.model.recipe.request.RecipesRequestModel
+import com.project.domain.model.recipe.response.RecipeDetailResponseModel
+import com.project.domain.usecase.image.ImageUploadUseCase
 import com.project.domain.usecase.recipe.DeleteRecipeUseCase
+import com.project.domain.usecase.recipe.GetRecipeDetailUseCase
 import com.project.domain.usecase.recipe.ModifyRecipeUseCase
 import com.project.presentation.viewmodel.util.UiState
 import com.project.presentation.viewmodel.util.exceptionHandling
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
 
 class ModifyViewModel(
     private val modifyRecipeUseCase: ModifyRecipeUseCase,
-    private val deleteRecipeUseCase: DeleteRecipeUseCase
+    private val deleteRecipeUseCase: DeleteRecipeUseCase,
+    private val getRecipeDetailUseCase: GetRecipeDetailUseCase,
+    private val imageUploadUseCase: ImageUploadUseCase
 ): ViewModel() {
     private val _modifyUiState: MutableStateFlow<UiState<Nothing>> = MutableStateFlow(UiState.Loading)
     val modifyUiState = _modifyUiState.asStateFlow()
@@ -21,6 +27,23 @@ class ModifyViewModel(
     private val _deleteRecipeUiState: MutableStateFlow<UiState<Nothing>> = MutableStateFlow(UiState.Loading)
     val deleteRecipeUiState = _deleteRecipeUiState.asStateFlow()
 
+    private val _recipeUiState: MutableStateFlow<UiState<RecipeDetailResponseModel>> = MutableStateFlow(UiState.Loading)
+    val recipeUiState = _recipeUiState.asStateFlow()
+
+    fun getRecipeDetail(index: Long) {
+        viewModelScope.launch {
+            getRecipeDetailUseCase(index)
+                .onSuccess {
+                    _recipeUiState.value = UiState.Success(it)
+                }.onFailure {
+                    it.exceptionHandling(
+                        unauthorizedAction = { _recipeUiState.value = UiState.Unauthorized },
+                        notFoundAction = { _recipeUiState.value = UiState.NotFound }
+                    )
+                }
+        }
+
+    }
     fun modifyRecipe(
         index:Long,
         body: RecipesRequestModel
@@ -54,6 +77,14 @@ class ModifyViewModel(
                         notFoundAction = { _deleteRecipeUiState.value = UiState.NotFound }
                     )
                 }
+        }
+    }
+
+    fun imageUpload(file: List<MultipartBody.Part>) {
+        viewModelScope.launch {
+            imageUploadUseCase(file)
+                .onSuccess { }
+                .onFailure { }
         }
     }
 }
