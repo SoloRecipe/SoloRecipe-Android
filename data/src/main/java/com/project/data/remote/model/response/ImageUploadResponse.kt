e7e0eadb8c6fd74ffe19c0dbd885d482efdef40e
박성현 <pjhomg5530@naver.com>
2023-06-12 23:08:11 +0900

    :recycle: url -> images로 변경

--- data/src/main/java/com/project/data/remote/model/response/ImageUploadResponse.kt
+++ data/src/main/java/com/project/data/remote/model/response/ImageUploadResponse.kt
@@ -3,9 +3,9 @@ package com.project.data.remote.model.response
 import com.project.domain.model.profile.response.ImageUploadResponseModel
 
 data class ImageUploadResponse(
-    val url: List<String>
+    val images: List<String>
 )
 
 fun ImageUploadResponse.asImageUploadResponseModel() = ImageUploadResponseModel(
-    url = url
+    images = images
 )
