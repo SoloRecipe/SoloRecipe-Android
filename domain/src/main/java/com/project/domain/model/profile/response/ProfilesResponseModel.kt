01fbf6b9b418befcb0d7f8b5acad4d198688269d
박성현 <pjhomg5530@naver.com>
2023-06-12 23:08:30 +0900

    :recycle: profileImg 추가

--- domain/src/main/java/com/project/domain/model/profile/response/ProfilesResponseModel.kt
+++ domain/src/main/java/com/project/domain/model/profile/response/ProfilesResponseModel.kt
@@ -2,6 +2,7 @@ package com.project.domain.model.profile.response
 
 data class ProfilesResponseModel(
     val name: String,
+    val profileImg: String,
     val likeRecipe: List<ProfileResponseModel>,
     val myRecipe: List<ProfileResponseModel>
 )
