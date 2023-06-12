5491c9d9a6f4fc3e85078d9fab2e164c82a272cf
hyeonseo <s21035@gsm.hs.kr>
2023-06-13 00:31:04 +0900

    :fire: :: 불필요한 TextField 삭제

--- presentation/src/main/java/com/project/presentation/view/modify/ModifyScreen.kt
+++ presentation/src/main/java/com/project/presentation/view/modify/ModifyScreen.kt
@@ -3,9 +3,7 @@ package com.project.presentation.view.modify
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,17 +18,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.project.design_system.component.SoloRecipeAppBar
 import com.project.design_system.component.SoloRecipeButton
-import com.project.design_system.theme.Body2
 import com.project.design_system.theme.IcTrashcan
 import com.project.design_system.theme.SoloRecipeColor
 import com.project.domain.model.recipe.request.RecipeRequestModel
@@ -124,44 +116,6 @@ fun ModifyScreen(
         UiState.NotFound -> {}
         else -> {}
     }
-}
-@Composable
-fun TextField(
-    modifier: Modifier = Modifier,
-    value: String,
-    hint: String,
-    textColor: Color = SoloRecipeColor.Black,
-    textStyle: TextStyle,
-    onValueChanged: (String) -> Unit
-) {
-    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
-    var text by remember { mutableStateOf(value) }
-
-    BasicTextField(
-        value = text,
-        textStyle = mergedTextStyle,
-        cursorBrush = SolidColor(SoloRecipeColor.Primary10),
-        onValueChange = {
-            text = it
-            onValueChanged(it)
-        },
-        decorationBox = { innerTextField ->
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box {
-                    if (text.isEmpty()) {
-                        Body2(
-                            text = hint,
-                            textColor = SoloRecipeColor.Secondary10,
-                        )
-                    }
-                    innerTextField()
-                }
-            }
-        }
-    )
 }
 
 @Composable
