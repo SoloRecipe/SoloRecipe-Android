5e43bf8f427856aa86e364638be699cddaf85abe
박성현 <pjhomg5530@naver.com>
2023-06-13 15:50:19 +0900

    :sparkles: imageUpload 로직 구현

--- presentation/src/main/java/com/project/presentation/viewmodel/registration/RegistrationViewModel.kt
+++ presentation/src/main/java/com/project/presentation/viewmodel/registration/RegistrationViewModel.kt
@@ -1,7 +1,9 @@
 package com.project.presentation.viewmodel.registration
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.project.domain.model.profile.response.ImageUploadResponseModel
 import com.project.domain.model.recipe.request.RecipesRequestModel
 import com.project.domain.usecase.image.ImageUploadUseCase
 import com.project.domain.usecase.recipe.CreateRecipeUseCase
@@ -22,6 +24,10 @@ class RegistrationViewModel @Inject constructor(
     private val _createUiState: MutableStateFlow<UiState<Nothing>> = MutableStateFlow(UiState.Loading)
     val createUiState = _createUiState.asStateFlow()
 
+    private val _modifyUiState: MutableStateFlow<UiState<ImageUploadResponseModel>> = MutableStateFlow(UiState.Loading)
+    val modifyUiState = _modifyUiState.asStateFlow()
+    val recipeImages = mutableListOf<String>("")
+
     fun createRecipe(body: RecipesRequestModel) {
         viewModelScope.launch {
             createRecipeUseCase(body)
@@ -37,11 +43,19 @@ class RegistrationViewModel @Inject constructor(
         }
     }
 
-    fun imageUpload(file: List<MultipartBody.Part>) {
+    fun imageUpload(file: List<MultipartBody.Part>, index: Int) {
         viewModelScope.launch {
             imageUploadUseCase(file)
-                .onSuccess { }
-                .onFailure { }
+                .onSuccess {
+                    recipeImages[index] = it.images[0]
+                    _modifyUiState.value = UiState.Success()
+                }
+                .onFailure {
+                    it.exceptionHandling(
+                        badRequestAction = { _modifyUiState.value = UiState.BadRequest },
+                        unauthorizedAction = { _modifyUiState.value = UiState.Unauthorized }
+                    )
+                }
         }
     }
 
